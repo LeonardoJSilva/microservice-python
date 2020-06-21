b045a5372a5c03e32c9c2147aa0afcea27be8e5e
leonardo silva <leonsilva700@gmail.com>
2020-06-21 18:35:29 -0300

    Create dockerfile

--- src/database/database.py
+++ src/database/database.py
@@ -1,3 +1,4 @@
+import logging
 import pymongo
 
 from database.settings import (
@@ -9,15 +10,22 @@ from database.settings import (
     MONGO_DB,
 )
 
-if MONGO_USER and MONGO_PASS:
-    client = pymongo.MongoClient(
-        "mongodb://{}:{}@{}:{}/{}".format(
-            MONGO_USER, MONGO_PASS, MONGO_HOST, MONGO_PORT, MONGO_ARGS
-        ),
-    )
-else:
-    client = pymongo.MongoClient(
-        "mongodb://{}:{}/{}".format(MONGO_HOST, MONGO_PORT, MONGO_ARGS),
-    )
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y - %I:%M:%S %p',
+                    level=logging.INFO)
+logging.info("Connecting in Database")
 
-db = client[MONGO_DB]
+try:
+    if MONGO_USER and MONGO_PASS:
+        logging.info(f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_ARGS}")
+        client = pymongo.MongoClient(
+            f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_ARGS}",
+        )
+    else:
+        logging.info(f"mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_ARGS}")
+        client = pymongo.MongoClient(
+            f"mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_ARGS}",
+        )
+    db = client[MONGO_DB]
+
+except Exception:
+    logging.error("Fail to connect in Database")
--- src/database/utils.py
+++ src/database/utils.py
@@ -2,13 +2,14 @@ from umongo.document import MetaDocumentImplementation, DocumentImplementation
 import logging
 
 logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
 
 
 def convert_to_mongo_document(object_to_convert: object, mongo_entity: MetaDocumentImplementation) -> [DocumentImplementation, None]:
     entity_db = mongo_entity()
     list_attr = list(object_to_convert.__dict__.keys())
     list_values = list(object_to_convert.__dict__.values())
-    logger.error(f"Converting from {object_to_convert.__class__.__name__} to {mongo_entity.__name__}")
+    logger.info(f"Converting from {object_to_convert.__class__.__name__} to {mongo_entity.__name__}")
     try:
         [entity_db.__setattr__(list_attr[item], list_values[item]) for item in range(0, len(list_attr))]
 
